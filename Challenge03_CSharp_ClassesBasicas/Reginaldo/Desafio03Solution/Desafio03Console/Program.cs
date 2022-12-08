963ce754ce2c87fadb988430d9d49bb89daab92f
reginaldosenai <113715701+reginaldosenai@users.noreply.github.com>
2022-12-08 10:03:19 -0300

    Solução criada console e TDD

---
+++ Challenge03_CSharp_ClassesBasicas/Reginaldo/Desafio03Solution/Desafio03Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Desafio03Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Challenge03_CSharp_ClassesBasicas/Reginaldo/Desafio03Solution/Desafio03TDD/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Desafio03TDD
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
